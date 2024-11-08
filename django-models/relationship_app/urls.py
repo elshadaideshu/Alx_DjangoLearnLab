ee786239d2075bf229671087e588ba587d6f6ef7
elshadaideshu <elshadaid48@gmail.com>
2024-11-08 08:47:23 -0800

    Add files via upload

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for the class-based view
     path('register/', RegisterView.as_view(), name='register'),  # URL for user registration
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),  # URL for user login
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),  # URL for user logout
+    path('logout/', LogoutView.as_view(template_name='registration/logged_out.html', next_page='login'), name='logout'),  # URL for user logout
 ]
