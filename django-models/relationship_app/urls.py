e74a840eaf9aef88e56a84654b46ec6123e1dc99
elshadaideshu <elshadaid48@gmail.com>
2024-11-08 09:21:03 -0800

    Add files via upload

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,9 +3,11 @@ from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books, LibraryDetailView, RegisterView  # Import your custom views
 
 urlpatterns = [
+    ["views.register"]
     path('books/', list_books, name='list_books'),  # URL for listing all books
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for library details
     path('register/', RegisterView.as_view(), name='register'),  # URL for user registration
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),  # URL for user login
     path('logout/', LogoutView.as_view(template_name='registration/logged_out.html', next_page='login'), name='logout'),  # URL for user logout
 ]
+
