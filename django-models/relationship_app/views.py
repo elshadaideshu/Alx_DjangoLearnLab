0eb986a62f64249a891fc02c00450bce0a4aa477
elshadaideshu <elshadaid48@gmail.com>
2024-11-08 09:52:01 -0800

    Add files via upload

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,16 +1,30 @@
-from django.shortcuts import render
-from .models import Book
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from .models import Book  # Import your Book model
 
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()  # Query all books from the database
-    return render(request, 'list_books.html', {'books': books})
-from django.views import View
-from django.shortcuts import get_object_or_404
-from .models import Library
+    return render(request, 'relationship_app/list_books.html', {'books': books})  # Render the template with the books context
 
 # Class-based view to display library details
 class LibraryDetailView(View):
     def get(self, request, pk):
         library = get_object_or_404(Library, pk=pk)  # Get the library by primary key
         return render(request, 'library_detail.html', {'library': library})
+
+# User Registration View
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'registration/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user after registration
+            return redirect('list_books')  # Redirect to the list of books after registration
+        return render(request, 'registration/register.html', {'form': form})
