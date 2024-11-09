f4c814737b1dd567c7874c9f73be4613c5dc4d81
elshadaideshu <elshadaid48@gmail.com>
2024-11-09 10:15:17 -0800

    Add files via upload

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,6 +6,33 @@ from .models import Book, Library
 from .models import Library
 from django.views.generic.detail import DetailView # Import both Book and Library models
 "relationship_app/register.html"
+# relationship_app/views.py
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseForbidden
+
+# Check if user is Admin
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+# Check if user is Librarian
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+# Check if user is Member
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 # Function-based view to list all books
 def list_books(request):
