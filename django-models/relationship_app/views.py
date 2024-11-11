89b7d8b36290414fb3efe7510a67964d24176f33
elshadaideshu <elshadaid48@gmail.com>
2024-11-11 08:37:59 -0800

    Add files via upload

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,63 +1,70 @@
 # relationship_app/views.py
-from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
-# relationship_app/views.py
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
 from django.contrib.auth.decorators import permission_required
+from django.utils.decorators import method_decorator
+from .forms import UserRegistrationForm, BookForm
 from .models import Book
-from .forms import BookForm  # Assuming you have a form for the Book model
+# relationship_app/admin_view.py
+from django.shortcuts import render
 
-@permission_required('relationship_app.can_add_book')
-def add_book(request):
-    if request.method == 'POST':
+def admin_view(request):
+    return render(request, 'relationship_app/admin.html')  # Ensure this template exists
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserRegistrationForm()
+        return render(request, 'relationship_app/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('list_books')
+        return render(request, 'relationship_app/register.html', {'form': form})
+
+class ListBooksView(View):
+    def get(self, request):
+        books = Book.objects.all()
+        return render(request, 'relationship_app/list_books.html', {'books': books})
+
+@method_decorator(permission_required('relationship_app.can_add_book'), name='dispatch')
+class AddBookView(View):
+    def get(self, request):
+        form = BookForm()
+        return render(request, 'relationship_app/add_book.html', {'form': form})
+
+    def post(self, request):
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('book_list')  # Redirect to a book list or another page
-    else:
-        form = BookForm()
-    return render(request, 'relationship_app/book_form.html', {'form': form})
+            return redirect('list_books')
+        return render(request, 'relationship_app/add_book.html', {'form': form})
 
-@permission_required('relationship_app.can_change_book')
-def edit_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    if request.method == 'POST':
+@method_decorator(permission_required('relationship_app.can_change_book'), name='dispatch')
+class EditBookView(View):
+    def get(self, request, book_id):
+        book = get_object_or_404(Book, id=book_id)
+        form = BookForm(instance=book)
+        return render(request, 'relationship_app/edit_book.html', {'form': form, 'book': book})
+
+    def post(self, request, book_id):
+        book = get_object_or_404(Book, id=book_id)
         form = BookForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
-            return redirect('book_list')  # Redirect after saving
-    else:
-        form = BookForm(instance=book)
-    return render(request, 'relationship_app/book_form.html', {'form': form})
+            return redirect('list_books')
+        return render(request, 'relationship_app/edit_book.html', {'form': form, 'book': book})
 
-@permission_required('relationship_app.can_delete_book')
-def delete_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    if request.method == 'POST':
+@method_decorator(permission_required('relationship_app.can_delete_book'), name='dispatch')
+class DeleteBookView(View):
+    def get(self, request, book_id):
+        book = get_object_or_404(Book, id=book_id)
+        return render(request, 'relationship_app/delete_book.html', {'book': book})
+
+    def post(self, request, book_id):
+        book = get_object_or_404(Book, id=book_id)
         book.delete()
-        return redirect('book_list')  # Redirect after deletion
-    return render(request, 'relationship_app/book_confirm_delete.html', {'book': book})
-
-# Check if user is Admin
-def is_admin(user):
-    return user.is_authenticated and user.userprofile.role == 'Admin'
-
-# Check if user is Librarian
-def is_librarian(user):
-    return user.is_authenticated and user.userprofile.role == 'Librarian'
-
-# Check if user is Member
-def is_member(user):
-    return user.is_authenticated and user.userprofile.role == 'Member'
-
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
-
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
+        return redirect('list_books')
