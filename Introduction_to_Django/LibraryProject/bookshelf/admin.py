c3ecf834e3fae01a497e06f7c0ffb48f286f16a6
elshadaideshu <elshadaid48@gmail.com>
2024-11-24 03:42:59 -0800

    Add files via upload

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,11 +1,15 @@
+# your_app/admin.py
 from django.contrib import admin
-from .models import Book
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Columns to display
-    list_filter = ('author', 'publication_year')  # Filters for the sidebar
-    search_fields = ('title', 'author')  # Searchable fields
-admin.site.register(Book, BookAdmin)
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
 
-
-# Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
