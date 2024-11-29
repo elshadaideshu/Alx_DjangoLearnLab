743a21061bbdd058e95afb5a322b955fa9359ad5
elshadaideshu <elshadaid48@gmail.com>
2024-11-29 01:51:32 -0800

    Add files via upload

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+# api/views.py
+
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()  # Use the Book model's queryset
+    serializer_class = BookSerializer  # Use the BookSerializer
 
 # Create your views here.
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# api_project/urls.py
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Include the API URLs
 ]
+
+
