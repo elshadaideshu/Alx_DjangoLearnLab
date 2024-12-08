2f1d9758a9601e6c9f7ab22d484972f83e530840
elshadaideshu <elshadaid48@gmail.com>
2024-12-08 03:42:02 -0800

    Add files via upload

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,13 +7,12 @@ from .views import register, user_login, user_logout, profile
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 
-
 urlpatterns = [
-    path('', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('', PostListView.as_view(), name='post-list'),  # List all posts
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # View a single post
+    path('post/new/', PostCreateView.as_view(), name='post-create'),  # Create a new post
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # Update an existing post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # Delete a post
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
