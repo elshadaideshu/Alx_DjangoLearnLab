2384ee026db7398d479f90d7fe12a18d98b99b3b
elshadaideshu <elshadaid48@gmail.com>
2024-12-12 04:52:02 -0800

    Add files via upload

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,6 +3,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+["get_user_model().objects.create_user"]
 
 User = get_user_model()
 
