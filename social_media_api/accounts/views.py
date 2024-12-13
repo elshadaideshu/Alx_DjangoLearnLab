dc0c89fa46b5fc505bc1fde2599877517379caa9
elshadaideshu <elshadaid48@gmail.com>
2024-12-13 01:41:12 -0800

    Add files via upload

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,15 +1,3 @@
-from rest_framework import generics
-from rest_framework.authtoken.views import ObtainAuthToken
-from .serializers import UserRegistrationSerializer, UserLoginSerializer
-# accounts/views.py
-
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework import viewsets
-from .models import Post
-from .serializers import PostSerializer
-from django.shortcuts import get_list_or_404
 # accounts/views.py
 
 from rest_framework import generics, permissions, status
@@ -17,6 +5,14 @@ from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+class UserListView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        users = User.objects.all()  # Fetch all users
+        user_data = [{"id": user.id, "username": user.username} for user in users]
+        return Response(user_data, status=status.HTTP_200_OK)
 
 class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -47,48 +43,3 @@ class FollowingListView(generics.GenericAPIView):
         following = request.user.following.all()
         following_usernames = [user.username for user in following]
         return Response({"following": following_usernames}, status=status.HTTP_200_OK)
-# posts/views.py
-
-
-
-class FeedView(viewsets.ReadOnlyModelViewSet):
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        user = self.request.user
-        return Post.objects.filter(author__in=user.following.all()).order_by('-created_at')
-
-User = get_user_model()
-
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def follow_user(request, user_id):
-    try:
-        user_to_follow = User.objects.get(id=user_id)
-        request.user.following.add(user_to_follow)
-        return Response({"message": "You are now following this user."}, status=status.HTTP_200_OK)
-    except User.DoesNotExist:
-        return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
-
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def unfollow_user(request, user_id):
-    try:
-        user_to_unfollow = User.objects.get(id=user_id)
-        request.user.following.remove(user_to_unfollow)
-        return Response({"message": "You have unfollowed this user."}, status=status.HTTP_200_OK)
-    except User.DoesNotExist:
-        return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
-
-class UserRegistrationView(generics.CreateAPIView):
-    serializer_class = UserRegistrationSerializer
-
-class UserLoginView(ObtainAuthToken):
-    serializer_class = UserLoginSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        token = serializer.create(serializer.validated_data)
-        return Response({'token': token})
